55f075d2238dfb800edf89a8e1337728178fb33a
Breno Garcia <BrenoGHeat@gmail.com>
2024-11-08 11:34:44 -0300

    update categories.controllers

--- src/controllers/categories.controllers.ts
+++ src/controllers/categories.controllers.ts
@@ -3,7 +3,6 @@ import { Request, Response } from "express";
 
 export class CategoriesControllers{
 
-
     async createCategory( req: Request, res: Response){
 
         const body = req.body;
@@ -12,4 +11,12 @@ export class CategoriesControllers{
 
         return res.status(201).json(category);
     }
+
+    async delete(req: Request, res: Response){
+       
+        await this.CategoriesServices.delete(Number(req.params.id));
+        
+        return res.status(204).json();
+      }
+
 }
